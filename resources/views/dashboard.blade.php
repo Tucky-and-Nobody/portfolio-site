361df9ab3e4817a87fdbc3e2c692b8e6ec89e9a4
Tucky_and_Nobody <nobody19990520@gmail.com>
2025-07-30 00:50:34 +0900

    concerts-information, blogs, performed-works, authentication

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,21 +1,17 @@
-@extends('layouts.app')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
 
-@section('content')
-
-<!-- ニュース抜粋 -->
-
-@include('for-dashboard.dashboard-concerts')
-
-@include('for-dashboard.dashboard-blogs')
-
-<!-- ファンクラブ案内 -->
-
-<!-- プロフィールリンク -->
-
-<!-- 演奏した楽曲リンク -->
-
-<!-- お問い合わせリンク -->
-
-<!-- -------------------------------------------------------- -->
-
-@endsection
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
